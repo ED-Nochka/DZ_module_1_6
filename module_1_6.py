c328ec97290f084ebe27c4ba48c28eeacd286b2d
ED-Nochka <nocevnayaed@gmail.com>
2024-10-03 11:59:02 +0500

    Add files via upload

--- module_1_6.py
+++ module_1_6.py
@@ -10,9 +10,9 @@ x = my_dict.pop('Mahsa')
 print(x)
 print(my_dict)
 # Множества
-my_set = {2,2.5,'a','b',2.5,'a'}
+my_set = {2, 2.5, 'a', 'b', 2.5, 'a'}
 print(my_set)
-my_set = {2,2.5,'a','b',2.5,'a', 3.5, 1}
+my_set = {2, 2.5, 'a', 'b', 2.5, 'a', 3.5, 1}
 print(my_set)
 print(my_set.discard(1))
 print(my_set)
